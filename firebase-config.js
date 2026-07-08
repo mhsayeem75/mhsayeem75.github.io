de3302d578521ec3e1bdd1b793b9c89fc28b3da9
Minhajul Haque Sayeem <minhajul.haque1900@gmail.com>
2026-07-09 00:20:15 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,19 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase Configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyAHgu_aVZM5WypubRY4Iv9SWcAxNIbrAQU",
   authDomain: "sayeem-813df.firebaseapp.com",
   projectId: "sayeem-813df",
-  storageBucket: "sayeem-813df.firebasestorage.app",
   messagingSenderId: "797429651042",
-  appId: "1:797429651042:web:2ade6fd8c9bf16e001e33b",
-  measurementId: "G-R7N9MEC5YR"
-  firebase.initializeApp(firebaseConfig);
+  appId: "1:797429651042:web:2ade6fd8c9bf16e001e33b"
+};
+
+
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
+
+
+// Firestore Database
 
 const db = firebase.firestore();
-};
