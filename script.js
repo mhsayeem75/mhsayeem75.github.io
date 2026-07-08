839eab73af1ca18cdd26f8fe395132b59a4fdcdd
Minhajul Haque Sayeem <minhajul.haque1900@gmail.com>
2026-07-09 00:27:29 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+db.collection("products").onSnapshot((snapshot)=>{
+
+let products = document.getElementById("productsData");
+
+products.innerHTML = "";
+
+
+snapshot.forEach((doc)=>{
+
+let data = doc.data();
+
+
+products.innerHTML += `
+
+<div class="card">
+
+<img src="${data.image}" class="product-img">
+
+
+<h3>${data.name}</h3>
+
+
+<h4>${data.price}</h4>
+
+
+<a class="btn"
+href="https://wa.me/8801601689809?text=I want to order ${data.name}">
+
+Order Now
+
+</a>
+
+
+</div>
+
+`;
+
+});
+
+
+});
 db.collection("products")
 .onSnapshot((snapshot)=>{
 
